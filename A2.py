29c5d40d4cbd70804d038573573c0c0cf9551468
Swift 3.0 <stelliabanda@gmail.com>
2026-08-02 09:15:43 +0200

    Add files via upload

--- A2.py
+++ A2.py
@@ -1,9 +1,30 @@
-import matplotlib.pyplot as plt
+# Import libraries
+
+import pandas as pd
 import numpy as np
-x=np.arange(1,11,1)
-y1=(2*x)+1
-y2=(2*x**x)+2
-plt.plot(x,y1,'g', linewidth=3,label='y=2x+1')
-plt.plot(x,y2,'r', linewidth=3,label='y=2x^2+2')
-plt.legend()
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Import dataset
+data = pd.read_csv('Titanic Dataset.csv')
+
+print(data.head())
+
+# Set plot style
+sns.set_style('whitegrid')
+
+# Countplot for feature survived
+sns.countplot(x='Gender', hue='Survived', data=data)
+
+# Costomize plots
+sns.countplot(x='Survived', data=data, palette='winter')
+
+# Countplot for embarked
+sns.countplot(x='Embarked', data=data)
+
+# Rotate the value labels and modify their font size
+
+sns.countplot(x='Embarked', data=data)
+plt.xticks(rotation=30, fontsize=20)
 plt.show()
+
